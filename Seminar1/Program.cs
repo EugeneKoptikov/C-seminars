007fdbf53960991422ecb8afad2eb0ce038722ae
EugenKoptikov <evgenkoptikov100500@gmail.com>
2022-06-29 21:27:33 +0300

    Exercise 2 completed!

---
+++ Seminar1/Program.cs
@@ -0,0 +1,24 @@
+﻿int a, b;
+Console.WriteLine("Программа для сравнения двух чисел, и нахождения среди них наибольшего. Для продолжения нажмите ENTER");
+Console.ReadLine();
+Console.WriteLine("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите еще число для сравнения: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) 
+{
+	Console.Write("Наибольшее число - ");
+	Console.Write(a);
+}
+
+if (a < b) 
+{
+	Console.Write("Наибольшее число - ");
+	Console.WriteLine(b);
+}
+
+if (a == b) 
+{
+	Console.WriteLine("Введеные чила равны.");
+}
