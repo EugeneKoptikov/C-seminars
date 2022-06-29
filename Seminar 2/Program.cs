a84cee36f19063e4d973053f2973aa17f4d2a911
EugenKoptikov <evgenkoptikov100500@gmail.com>
2022-06-29 23:51:08 +0300

    Exercise 4 completed!

--- Seminar 2/Program.cs
+++ Seminar 2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Программа для сравнения двух чисел, и нахождения среди них наибольшего. Для продолжения нажмите ENTER");
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите еще число для сравнения: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число для сравнения: ");
+int e = Convert.ToInt32(Console.ReadLine());
+
+
+if (a > b && a > e) 
+{
+	Console.WriteLine("Наибольшее число - " + a);
+}
+    else if (a < b && b > e) 
+    {
+    	Console.WriteLine("Наибольшее число - " + b);
+    }
+        else if (a < e && b < e) 
+        {
+	        Console.WriteLine("Наибольшее число - " + e);
+        }
+            else if (a == b && b == e)
+            {
+                Console.WriteLine("Введенные числа равны");
+            }
